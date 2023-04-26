f5f620b5bc1be20c331332b04529e138d9a31ef4
hocinebouarara <hocinbouarara08@gmail.com>
2023-04-26 06:36:19 +0100

    added the PreAuthorize for adding permissions

--- src/main/java/com/hocinebouarara/springsecurity/student/StudentManagementController.java
+++ src/main/java/com/hocinebouarara/springsecurity/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.hocinebouarara.springsecurity.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -17,24 +18,28 @@ public class StudentManagementController {
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents(){
         System.out.println("getAllStudents");
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student){
         System.out.println("registerNewStudent");
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId){
         System.out.println("deleteStudent");
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId,@RequestBody Student student){
         System.out.println("updateStudent");
         System.out.println(String.format("%s %s",studentId,student));
